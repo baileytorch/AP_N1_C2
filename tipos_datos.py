da94c6672370963a1715f7654035b45bfd8dd6fe
baileytorch <baileytorch@hotmail.com>
2026-03-31 10:53:42 -0300

    Primera carga de codigo

---
+++ tipos_datos.py
@@ -0,0 +1,13 @@
+# Tipos de datos en Python
+
+numero_real = 0
+numero_decimal = 0.0
+cadena_texto = "Buen día queridos estudiantes!"
+
+print("Mi primer mensaje en Python")
+print(numero_real)
+print(type(numero_real))
+print(numero_decimal)
+print(type(numero_decimal))
+print(cadena_texto)
+print(type(cadena_texto))
