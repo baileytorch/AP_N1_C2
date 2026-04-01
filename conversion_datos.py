5c163d7de4df6e73d61b77e79541c4de51014ffa
baileytorch <baileytorch@hotmail.com>
2026-04-01 10:49:00 -0300

    Agregando nuevo archivo

---
+++ conversion_datos.py
@@ -0,0 +1,10 @@
+variable_logica = False
+print(variable_logica)
+print(type(variable_logica))
+
+texto_variable_logica = 'True'
+print(texto_variable_logica)
+print(type(texto_variable_logica))
+
+print(type(bool(texto_variable_logica)))
+print(texto_variable_logica)
