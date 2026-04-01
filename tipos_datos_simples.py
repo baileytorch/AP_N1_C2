eb33a08deb33d582eee870d03c86e8478f85369b
baileytorch <baileytorch@hotmail.com>
2026-04-01 09:50:14 -0300

    actualizando tipos de datos

---
+++ tipos_datos_simples.py
@@ -0,0 +1,57 @@
+# Tipos de datos en Python
+
+print(type(5))
+print(type(3.2))
+print(type('Hola'))
+print(type(True))
+
+# La variable numero_real almacenará un dato de número entero
+numero_entero = 0
+numero_decimal = 0.0
+cadena_texto = "Buen día queridos estudiantes!"
+valor_booleano = False
+
+print("Mi primer mensaje en Python")
+
+print()
+print(numero_entero)
+print(type(numero_entero))
+
+print()
+print(numero_decimal)
+print(type(numero_decimal))
+
+print()
+print(cadena_texto)
+print(type(cadena_texto))
+
+print()
+print(valor_booleano)
+print(type(valor_booleano))
+
+valor_booleano = 5 > 3
+print()
+print(valor_booleano)
+print(type(valor_booleano))
+
+# CONCATENACIÓN de cadenas de texto
+saludo = 'Buen día querido '
+nombre = 'Erick Bailey'
+print()
+print(saludo + nombre)
+
+texto_una_linea_comillas_simples = 'texto de una línea'
+texto_una_linea_comilla_dobles = "texto de una línea"
+
+texto_multiples_lineas_comillas_simples = '''
+línea 1 texto de múltiples líneas
+línea 2 texto de múltiples líneas
+línea 3 texto de múltiples líneas
+'''
+texto_multiples_lineas_comilla_dobles = """
+línea 1 texto de múltiples líneas
+línea 2 texto de múltiples líneas
+línea 3 texto de múltiples líneas
+"""
+
+print(texto_multiples_lineas_comilla_dobles)
---
+++ variables.py
@@ -0,0 +1,6 @@
+saludo = 'Buen día querido '
+nombre = input('Ingrese su nombre: ')
+print()
+print(saludo + nombre)
+
+# Ingrese 2 números mediante su teclado y muestre el resultado de la suma
