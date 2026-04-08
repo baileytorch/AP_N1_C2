94b2a25dfc56ec7d6a41c21142ea231d42ffc7c1
baileytorch <baileytorch@hotmail.com>
2026-04-08 10:13:13 -0400

    Inputs

---
+++ inputs.py
@@ -0,0 +1,9 @@
+# Cada dato ingresado medainte input ES un TEXTO
+
+numero = input('Por favor, ingrese un númereo entero...')
+print(type(numero))
+print(numero)
+
+print(f'Su NÚMERO multiplicado por 4 es : {numero * 4}')
+print(f'Su NÚMERO (convertido a decimal) multiplicado por 2 es : {float(numero) * 2}')
+print(f'Su NÚMERO (convertido a entero) multiplicado por 2 es : {int(numero) * 2}')
