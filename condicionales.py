8f7fd0bd218be0bf653026523d0a38d0317d1890
baileytorch <baileytorch@hotmail.com>
2026-04-15 10:19:17 -0400

    Condicionales

---
+++ condicionales.py
@@ -0,0 +1,14 @@
+str_edad = input('Ingrese su Edad: ')
+edad = int(str_edad)
+
+# Condicional IF
+if edad >= 18:
+    # Este set de acciones se ejecuta cuando la respuesta es V
+    print('Ud. es mayor de edad.')
+else:
+    # Este set de acciones se ejecuta cuando la respuesta es F
+    print('Ud. es menor de edad.')
+
+# Solicite al usuario el ingreso de datos personales (nombre, edad y título)
+# Si el usuario es mayor de edad, muestre por pantalla todos sus datos
+# Si el usuario NO es mayor de edad, muestre un mensaje indicando que es menor de edad.
